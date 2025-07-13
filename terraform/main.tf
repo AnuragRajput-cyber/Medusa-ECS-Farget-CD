565a9a5638b24739d87a26e7cc4645b952456148
AnuragRajput-cyber <anuragrajput2603@gmail.com>
2025-07-13 11:01:22 +0530

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,128 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.aws_region}b"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = var.container_port
+    to_port     = var.container_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  backend "s3" {
-    bucket = "terraform-state-bucket-v2-anurag"
-    key    = "medusa/terraform.tfstate"
-    region = "us-east-1"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "this" {
+  name = "${var.app_name}-cluster"
+}
+
+resource "aws_rds_instance" "postgres" {
+  identifier        = "${var.app_name}-db"
+  engine            = "postgres"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  name              = "medusadb"
+  username          = "medusa"
+  password          = var.db_password
+  skip_final_snapshot = true
+  publicly_accessible = true
+}
+
+resource "aws_lb" "this" {
+  name               = "${var.app_name}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+resource "aws_lb_target_group" "this" {
+  name     = "${var.app_name}-tg"
+  port     = var.container_port
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = "${var.app_name}-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+
+  container_definitions = jsonencode([
+    {
+      name      = "medusa"
+      image     = "<AWS_ECR_REPO>:latest"
+      portMappings = [
+        {
+          containerPort = var.container_port
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "DATABASE_URL"
+          value = "postgresql://medusa:${var.db_password}@${aws_rds_instance.postgres.address}:5432/medusadb"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "this" {
+  name            = "${var.app_name}-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.this.arn
+    container_name   = "medusa"
+    container_port   = var.container_port
   }
 }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,7 +1,22 @@
-provider "aws" {
-  region = "us-east-1"
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "us-east-1"
 }
-variable "DB_password" {
-  description = "RDS database password"
-  type        = string
+
+variable "app_name" {
+  description = "Name prefix for resources"
+  default     = "medusa"
+}
+
+variable "container_port" {
+  default = 9000
+}
+
+variable "desired_count" {
+  default = 1
+}
+
+variable "db_password" {
+  description = "Postgres DB password"
+  sensitive   = true
 }
