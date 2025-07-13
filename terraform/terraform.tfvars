7f7ff161cd5a7afad8e145a2357f958650f21614
AnuragRajput-cyber <anuragrajput2603@gmail.com>
2025-07-13 12:38:19 +0530

    update

--- terraform/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-ecr_repo = "061039787029.dkr.ecr.us-east-1.amazonaws.com/medusa-repo"
-db_password = "Anonimus214365"
--- terraform/variable.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-variable "DB_password" {
-  description = "RDS database password"
-  type        = string
-}
